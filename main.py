aeb943e4c94f473f776420396d9e4e0505ac8a40
Keeeal <james@keal.com.au>
2019-05-06 16:40:03 +0930

    log_board function

--- main.py
+++ main.py
@@ -2,7 +2,20 @@
 
 import sys, random, logging
 from util import *
-from board_logger import log_board
+
+def log_board(field, indent='  '):
+    logging.debug('')
+    for n, row in enumerate(field._Field__mBoard):
+        if n:
+            if n % 3:
+                sep = '---+---+---'
+                logging.debug(indent + sep + '‖' + sep + '‖' + sep)
+            else:
+                sep = '==========='
+                logging.debug(indent + sep + '#' + sep + '#' + sep)
+        row = ' ‖ '.join(' | '.join(row[i:i+3]) for i in range(0, len(row), 3))
+        logging.debug(indent + ' ' + row.replace('0','O').replace('1','X'))
+    logging.debug('')
 
 class Bot:
     def __init__(self):
@@ -18,7 +31,9 @@ class Bot:
         log_board(field)
 
         if len(moves):
-            return random.choice(moves)
+            move = random.choice(moves)
+            logging.info((move.getX(), move.getY()))
+            return move
 
 if __name__ == '__main__':
     bot = Bot()
